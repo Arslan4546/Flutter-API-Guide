f609dccb04c8a88ba28ead5543488b3908baa8a7
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-15 14:51:04 +0500

    update

--- flutter_api_guide/lib/main.dart
+++ flutter_api_guide/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter_api_guide/Views/Get_API/Complex_Json_witStarting_with_Ob
 import 'package:flutter_api_guide/Views/Get_API/Create_Custom_Model_With_GetAPI/custom_model_screen.dart';
 import 'package:flutter_api_guide/Views/Get_API/If_Model_Not_Possible/model_not_possible.dart';
 import 'package:flutter_api_guide/Views/Get_API/Simple_GetAPI_with_Posts_Model/simple_post_api_screen.dart';
+import 'package:flutter_api_guide/Views/Login_SignUP_Post_API/login_signup_api.dart';
+import 'package:flutter_api_guide/Views/Post_Image_From_Gallery_API/post_image_from_gallery.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,7 +25,8 @@ class MyApp extends StatelessWidget {
       //home: CustomModelScreen(),
       //home: ComplexJsonUsingExtension()
       // home: ModelNotPossible(),
-      home: Withoutstartinglistbutobject(),
+      // home: LoginSignUpScreen(),
+      home: PostImage(),
     );
   }
 }

2c332418de6db1a232df7f9ed782b276167af1cf
Arslan Tariq <arslantariq4546@gmail.com>
2025-07-15 13:57:18 +0500

    without list but starting with json object done

--- flutter_api_guide/lib/main.dart
+++ flutter_api_guide/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_api_guide/Views/Complex_Json_With_Extension/complex_json_using_extension.dart';
-import 'package:flutter_api_guide/Views/Create_Custom_Model_With_GetAPI/custom_model_screen.dart';
-import 'package:flutter_api_guide/Views/Simple_GetAPI_with_Posts_Model/simple_post_api_screen.dart';
+import 'package:flutter_api_guide/Views/Get_API/Complex_Json_With_Extension/complex_json_using_extension.dart';
+import 'package:flutter_api_guide/Views/Get_API/Complex_Json_witStarting_with_Object/withoutStartingListButObject.dart';
+import 'package:flutter_api_guide/Views/Get_API/Create_Custom_Model_With_GetAPI/custom_model_screen.dart';
+import 'package:flutter_api_guide/Views/Get_API/If_Model_Not_Possible/model_not_possible.dart';
+import 'package:flutter_api_guide/Views/Get_API/Simple_GetAPI_with_Posts_Model/simple_post_api_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +21,9 @@ class MyApp extends StatelessWidget {
       ),
       //home: SimplePostApiScreen(),
       //home: CustomModelScreen(),
-      home: ComplexJsonUsingExtension(),
+      //home: ComplexJsonUsingExtension()
+      // home: ModelNotPossible(),
+      home: Withoutstartinglistbutobject(),
     );
   }
 }
